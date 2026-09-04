2ca45d774d8dd8fe3f210718df7adc96a7402e8b
NicolasF <lopezfn25@gmail.com>
2026-09-04 00:28:17 -0500

    main creacion de objetos superclase

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+        // ============ OBJETOS DE LA SUPERCLASE (Deportista) ============
+
+        // Se usa el constructor de dos atributos porque en este caso se registra un
+        // deportista genérico del que todavía se conoce solo el nombre y la edad.
+        Deportista d1 = new Deportista("Roberto Carlos", 32);
+
+        // Se usa el constructor de 4 parámetros porque ya se cuenta con
+        // toda la información completa de este deportista.
+        Deportista d2 = new Deportista("Ricardo Peña", 30, true, 78.5);
+
+
+
 }
