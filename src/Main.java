81f865574b700b899c403e716ec7b6940a992d7f
NicolasF <lopezfn25@gmail.com>
2026-09-04 00:29:05 -0500

    main creacion de objetos subclase tenista

--- src/Main.java
+++ src/Main.java
@@ -27,6 +27,17 @@ public class Main {
         // con toda la información física y deportiva del jugador.
         Futbolista f3 = new Futbolista("Luis Torres", 24, true, 72.0, "Delantero", 15, 28.5, true);
 
+// ============ OBJETOS DE LA SUBCLASE Tenista ============
+
+        // Se usa el constructor de 2 parámetros porque de este tenista
+        // solo se tienen el nombre y la edad al momento de registrarlo.
+        Tenista t1 = new Tenista("Valentina Rojas", 24);
+
+        // Se usa el constructor de 8 parámetros (completo) porque ya se
+        // conocen todos los datos físicos y deportivos de la tenista.
+        Tenista t2 = new Tenista("Camila Duarte", 26, true, 61.0, "Zurda", 15, 68.4, true);
+
+
 
 
 }
