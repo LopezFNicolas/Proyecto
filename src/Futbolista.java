d6e8375525e54ac7c6671393b710daa4cca9ad55
NicolasF <lopezfn25@gmail.com>
2026-09-03 23:32:05 -0500

    Subclase Futbolista

---
+++ src/Futbolista.java
@@ -0,0 +1,10 @@
+public class Futbolista {
+    private String posicion;
+    private int goles;
+    private double velocidadPromedio;
+    private boolean esCapitan;
+
+    public Futbolista(){
+        this("posicion",);
+    }
+}
